2eddd40dfb32e36f28599abb0ff52d78eafd4f3b
SVD-NL <72652479+SVD-NL@users.noreply.github.com>
2023-06-23 16:55:51 +0200

    Add update coordinators

--- custom_components/helios2n/__init__.py
+++ custom_components/helios2n/__init__.py
@@ -1,19 +1,28 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, Platform
 
 from py2n import Py2NDevice, Py2NConnectionData
 
-from .const import DOMAIN, PLATFORMS
+from .const import DOMAIN
+from .coordinator import Helios2nPortDataUpdateCoordinator, Helios2nSwitchDataUpdateCoordinator
+
+platforms = [Platform.BUTTON, Platform.LOCK, Platform.SWITCH, Platform.BINARY_SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, config: ConfigType) -> bool:
 	aiohttp_session = async_get_clientsession(hass)
 	connection_data = Py2NConnectionData(host= config.data[CONF_HOST], username=config.data[CONF_USERNAME], password=config.data[CONF_PASSWORD])
-	hass.data.setdefault(DOMAIN,{})[config.entry_id] = await Py2NDevice.create(aiohttp_session, connection_data)
+	device = await Py2NDevice.create(aiohttp_session, connection_data)
+	hass.data.setdefault(DOMAIN,{})[config.entry_id] = device
+	for platform in platforms:
+		hass.data[DOMAIN].setdefault(platform, {})
+	hass.data[DOMAIN][Platform.LOCK]["coordinator"] = Helios2nSwitchDataUpdateCoordinator(hass, device)
+	hass.data[DOMAIN][Platform.SWITCH]["coordinator"] = Helios2nPortDataUpdateCoordinator(hass, device)
+	hass.data[DOMAIN][Platform.BINARY_SENSOR]["coordinator"] = Helios2nPortDataUpdateCoordinator(hass, device)
 	hass.async_create_task(
 		hass.config_entries.async_forward_entry_setups(
-		config, PLATFORMS
+		config, platforms
 		)
 	)
 	return True
--- custom_components/helios2n/binary_sensor.py
+++ custom_components/helios2n/binary_sensor.py
@@ -1,33 +1,37 @@
 import logging
-from typing import Any, Coroutine
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.const import Platform
 
 from py2n import Py2NDevice
 
 from .const import DOMAIN
+from .coordinator import Helios2nPortDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
+PLATFORM = Platform.BINARY_SENSOR
 
 async def async_setup_entry(hass: HomeAssistant, config: ConfigType, async_add_entities: AddEntitiesCallback):
-    device: Py2NDevice
     device: Py2NDevice = hass.data[DOMAIN][config.entry_id]
+    coordinator: Helios2nPortDataUpdateCoordinator = hass.data[DOMAIN][PLATFORM]["coordinator"]
     entities = []
     for port in device.data.ports:
         if port.type == "input":
-            entities.append(Helios2nPortBinarySensorEntity(device, port.id))
+            entities.append(Helios2nPortBinarySensorEntity(coordinator, device, port.id))
     async_add_entities(entities)
     return True
 
-class Helios2nPortBinarySensorEntity(BinarySensorEntity):
+class Helios2nPortBinarySensorEntity(CoordinatorEntity, BinarySensorEntity):
     _attr_has_entity_name = True
     _attr_entity_registry_enabled_default = False
 
-    def __init__(self, device: Py2NDevice, port_id: str) -> None:
+    def __init__(self, coordinator: Helios2nPortDataUpdateCoordinator, device: Py2NDevice, port_id: str) -> None:
+        super().__init__(coordinator)
         self._device = device
         self._attr_unique_id = f"{self._device.data.serial}_port_{port_id}"
         self._attr_name = port_id
@@ -50,6 +54,3 @@ class Helios2nPortBinarySensorEntity(BinarySensorEntity):
         for port in self._device.data.ports:
             if port.id == self._port_id:
                 return port.state
-
-    async def async_update(self):
-        await self._device.update_port_status()
